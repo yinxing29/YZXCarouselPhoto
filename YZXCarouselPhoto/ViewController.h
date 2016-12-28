1315d3b27e0900cf343856f1e161d6ef8ab75c7b
yinxing29 <yinxing29@sina.com>
2016-12-28 16:20:46 +0800

    Initial Commit

---
+++ YZXCarouselPhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YZXCarouselPhoto
+//
+//  Created by 尹星 on 2016/12/28.
+//  Copyright © 2016年 yinixng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YZXCarouselPhoto/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  YZXCarouselPhoto
+//
+//  Created by 尹星 on 2016/12/28.
+//  Copyright © 2016年 yinixng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
